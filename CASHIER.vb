2ee9ef77e4723d5c9c429168fa41c6d32e8a64f0
Valdez_JSHTN <z3thsu0@gmail.com>
2025-04-12 03:40:11 +0800

    up dit, sales-->dashboard

--- CASHIER.vb
+++ CASHIER.vb
@@ -79,9 +79,6 @@ Public Class CASHIER
         End Try
     End Sub
 
-    Private Sub Guna2Button2_Click(sender As Object, e As EventArgs) Handles Guna2Button2.Click
-        RECEIPT.Show()
-    End Sub
 
 
     Private Sub UpdateItemQuantity(productName As String, newQuantity As Integer, newTotal As Decimal)
@@ -154,4 +151,7 @@ Public Class CASHIER
         End If
     End Sub
 
+    Private Sub Guna2Button2_Click(sender As Object, e As EventArgs) Handles Guna2Button2.Click
+        RECEIPT.Show()
+    End Sub
 End Class
--- RECEIPT.vb
+++ RECEIPT.vb
@@ -1,12 +1,2 @@
-﻿Imports CrystalDecisions.CrystalReports.Engine
-
-Public Class RECEIPT
-    Private Sub RECEIPT_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim report As New ReportDocument()
-        report.Load("YourReportPath.rpt")
-        report.SetDataSource(reportData)
-
-        CrystalReportViewer1.ReportSource = report
-        CrystalReportViewer1.RefreshReport()
-    End Sub
+﻿Public Class RECEIPT
 End Class
