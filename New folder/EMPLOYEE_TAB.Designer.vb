c544518f34dd970166cbe971d16aadabc60d9557
human-person13 <raileypoblete04@gmail.com>
2025-04-24 03:23:01 +0800

    Add files via upload

---
+++ New folder/EMPLOYEE_TAB.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class EMPLOYEE_TAB
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SHITSTEMDataSet = New DIMSUM_SHITSTEM.SHITSTEMDataSet()
+        Me.LoginBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.LoginTableAdapter = New DIMSUM_SHITSTEM.SHITSTEMDataSetTableAdapters.loginTableAdapter()
+        Me.TableAdapterManager = New DIMSUM_SHITSTEM.SHITSTEMDataSetTableAdapters.TableAdapterManager()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.addemployeebtn = New Guna.UI2.WinForms.Guna2Button()
+        CType(Me.SHITSTEMDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LoginBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SHITSTEMDataSet
+        '
+        Me.SHITSTEMDataSet.DataSetName = "SHITSTEMDataSet"
+        Me.SHITSTEMDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'LoginBindingSource
+        '
+        Me.LoginBindingSource.DataMember = "login"
+        Me.LoginBindingSource.DataSource = Me.SHITSTEMDataSet
+        '
+        'LoginTableAdapter
+        '
+        Me.LoginTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.DailySalesTableAdapter = Nothing
+        Me.TableAdapterManager.DailySummaryTableAdapter = Nothing
+        Me.TableAdapterManager.loginTableAdapter = Me.LoginTableAdapter
+        Me.TableAdapterManager.MonthlySalesTableAdapter = Nothing
+        Me.TableAdapterManager.OrdersTableAdapter = Nothing
+        Me.TableAdapterManager.ProductGroupsTableAdapter = Nothing
+        Me.TableAdapterManager.STOCKSTableAdapter = Nothing
+        Me.TableAdapterManager.TransactionDetailsTableAdapter = Nothing
+        Me.TableAdapterManager.TransactionsTableAdapter = Nothing
+        Me.TableAdapterManager.UpdateOrder = DIMSUM_SHITSTEM.SHITSTEMDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        Me.TableAdapterManager.WeeklySalesTableAdapter = Nothing
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(34, 16)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(336, 343)
+        Me.Panel1.TabIndex = 3
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.AutoScroll = True
+        Me.FlowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.FlowLayoutPanel1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlowLayoutPanel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FlowLayoutPanel1.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(34, 384)
+        Me.FlowLayoutPanel1.Margin = New System.Windows.Forms.Padding(4)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(1556, 449)
+        Me.FlowLayoutPanel1.TabIndex = 4
+        '
+        'addemployeebtn
+        '
+        Me.addemployeebtn.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.addemployeebtn.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.addemployeebtn.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.addemployeebtn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.addemployeebtn.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.addemployeebtn.ForeColor = System.Drawing.Color.White
+        Me.addemployeebtn.Location = New System.Drawing.Point(667, 16)
+        Me.addemployeebtn.Margin = New System.Windows.Forms.Padding(4)
+        Me.addemployeebtn.Name = "addemployeebtn"
+        Me.addemployeebtn.Size = New System.Drawing.Size(235, 65)
+        Me.addemployeebtn.TabIndex = 4
+        Me.addemployeebtn.Text = "ADD EMPLOYEE"
+        '
+        'EMPLOYEE_TAB
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.Controls.Add(Me.addemployeebtn)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "EMPLOYEE_TAB"
+        Me.Size = New System.Drawing.Size(1631, 850)
+        CType(Me.SHITSTEMDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LoginBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents SHITSTEMDataSet As SHITSTEMDataSet
+    Friend WithEvents LoginBindingSource As BindingSource
+    Friend WithEvents LoginTableAdapter As SHITSTEMDataSetTableAdapters.loginTableAdapter
+    Friend WithEvents TableAdapterManager As SHITSTEMDataSetTableAdapters.TableAdapterManager
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents addemployeebtn As Guna.UI2.WinForms.Guna2Button
+End Class
